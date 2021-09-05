9019c79ab0a13ba43837b7a22f138c402c06a3f2
Austen Stone <miltonlaxer@gmail.com>
2021-09-05 08:52:56 -0400

    linting

--- src/app/components/special-button/special-button.component.ts
+++ src/app/components/special-button/special-button.component.ts
@@ -5,15 +5,12 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './special-button.component.html',
   styleUrls: ['./special-button.component.css']
 })
-export class SpecialButtonComponent implements OnInit {
+export class SpecialButtonComponent {
   @Input() href: string;
   @Input() img: string;
   @Input() icon: string;
   @Input() text: string;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
