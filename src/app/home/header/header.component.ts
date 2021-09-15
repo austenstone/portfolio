5145238b9126fec47d665fd405553d9d6a0a9f87
Austen Stone <miltonlaxer@gmail.com>
2021-09-15 12:53:16 -0400

    typing

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,6 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import * as Typed from 'typed.js';
 
+const ets = (emoji) => {
+  let emojiHex = '';
+  if (emoji.length === 2) {
+    emojiHex = emoji;
+  } else {
+    let comp = (
+      (emoji.charCodeAt(0) - 0xD800) * 0x400
+      + (emoji.charCodeAt(1) - 0xDC00) + 0x10000
+    );
+    if (comp < 0) {
+      emojiHex = emoji;
+    }
+    emojiHex = emoji;
+    return `&#x${emojiHex.codePointAt(0).toString(16)};&#x${emojiHex.codePointAt(2).toString(16)};&#x${emojiHex.codePointAt(3).toString(16)};`
+  }
+  return `&#x${emojiHex.codePointAt(0).toString(16)};`
+};
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -13,16 +30,17 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     const typed = new Typed.default('.skills', {
       strings: [
-        'Developer &amp; UI/UX Creator',
-        'Developer &amp; Designer',
-        'C/C++ Developer',
-        'App Creator',
-        'App Designer',
-        'Internet Of Things Developer',
-        'Internet Of Things Maker',
-        'Angular Developer',
-        'Angular Dev',
-        'Web Developer'
+        `Solutions Engineer `,
+        `Solutions Engineer ${ets('🚀')}`,
+        `Software Engineer ${ets('👨‍💻')}`,
+        `Developer &amp; UI/UX Creator ${ets('🔨')}`,
+        `App Creator ${ets('🔨')}`,
+        `App Designer 🎨`,
+        `Internet Of Things Maker ${ets('🔧')}`,
+        `Internet Of Things Connector ${ets('🌐')}`,
+        `Angular Developer`,
+        `Angular Dev`,
+        `Web Developer`
       ],
       typeSpeed: 50,
       backSpeed: 40,
