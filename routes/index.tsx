6b6c4bd4a97f67c7e0e778c3427e03b2149b6aa1
Austen Stone <austenstone@github.com>
2023-09-19 08:04:20 -0400

    temp

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,42 @@
 export default function Home() {
   return (
-    <h1 style="font-size:5em">Work</h1>
+    <div class="box-border grid grid-cols-[100%]">
+      <div class="w-full h-24" style="background-color:black;color:white;">
+        <div>
+          <div>Logo</div>
+        </div>
+        <div>
+          <h2>Work</h2>
+          <h2>About</h2>
+          <h2>Contact</h2>
+        </div>
+      </div>
+      <div class="w-full h-48 bg-center bg-cover" style="background-image:url('https://lh3.googleusercontent.com/pw/AIL4fc_iT24BcO-h4CA2wI4cvZi6BCqz338G70pOq-VEBkz-H7a4WJg0jF6w9FETSauOHu5HcjZM08gVTjTDA0_Hx_KRmaHkUMzhQTDDoSXn2NHM_OkVl_p0pLOZKjHf5cKU3kgzRx9oDfJyDLPJuA0MAIhkSQuFGyz-Id1SmFEQ0YYTVEzS5SwjNUFK6-MvZic3Tt7Oh29WvpdihP6OW_T5kGeH3Qpj1xkw_Ef0ESIILKSHLTqYg-WBmUQ5mb8oFRDJYN8HHVCLx9jO8znTbo7Yo0HP4BKTwPQSBU1pGEG_Upw88i45b917BM3R3CeE781wENjeEdb5iAJqv6c7bc0KD8gMvOYDpG_EWHB1Sf56tEGWLG2AtQN8_LzOEkuBBVDFAK5OIH8ST7jgmBwRlucX_CUeU92vT_8JchXBXU41-dbtSACLgpA85pQTJKm2dX0LH6exY7OhEPYAZlac82gYzrEQK4IOMfpfKFNUZ72JYqsT77gF7jrG2wvE3qvQcUJMFBAFVDPGFA8jY7tRAYU3_ZSOFadUv1-O5VcFg4g6Wn7HiB0h28X2z85EoVYPbMx_MBwVDRN5RPg0ZYW3hmMQ_GxKK0sVRzjqF2pJj17dXVciEP9igsptThsfYQEVIL_pk0PNujFVqPgvfqqmAwun3qf63xF23yvW4cGTdaACbdUGJI3ZBr-vW0NSljIgGEOJLLj6WlvTt2J1hqEgRk2tnh8T16xHP8Ljc7jFYBn01pLuKw2wRUz7ayBOFzSJYhMC40pdCa2hRHuC6xBUHUTWQztgZXernsUb_zXtbj0BYJx-xfn_bfg8on6BLGhqif-mnHb4EsNQtqJmL8n3aQlMzBSp6SrGJgAYvK9vJn1z_T_8THWxKc5CtynJA1M-O6tRpPRTUz1hDfy7rQ7k_Z02mBI=w1651-h1244-s-no?authuser=0');"></div>
+      <div class="ml-12">
+        <h1 class="mt-8 text-9xl font-extrabold">
+          Austen<br/>
+          Stone
+        </h1>
+        <div class="mb-12 text-2xl">
+          <h2>Design</h2>
+          <h2>Consulting</h2>
+          <h2>Engineering</h2>
+        </div>
+      </div>
+      <div class="ml-12">
+        <div class="mb-12 mt-14  grid grid-cols-[max-content+50%] items-baseline">
+          <h1 class="text-9xl font-extrabold">Hello! 👋</h1>
+          <p>I'm a Engineer currently working @GitHub</p>
+        </div>
+      </div>
+      <div class="w-full h-24"></div>
+      <div class="w-full h-24"></div>
+      <div class="w-full h-24"></div>
+      <div class="w-full h-24"></div>
+      <div class="w-full h-24"></div>
+      <div class="w-full h-24"></div>
+      <div class="w-full h-24"></div>
+      <div class="w-full h-24"></div>
+    </div>
   );
 }
--- twind.config.ts
+++ twind.config.ts
@@ -10,7 +10,6 @@ export default {
       }
     },
     preflight: {
-      // Import external stylesheet
       '@import': `url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap')`,
     },
   },
