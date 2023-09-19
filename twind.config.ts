64a8fa7369babf3710701a790e751e112851f434
Austen Stone <austenstone@github.com>
2023-09-18 21:14:16 -0400

    init

---
+++ twind.config.ts
@@ -0,0 +1,17 @@
+import { Options } from "$fresh/plugins/twind.ts";
+
+
+export default {
+  selfURL: import.meta.url,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif']
+      }
+    },
+    preflight: {
+      // Import external stylesheet
+      '@import': `url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap')`,
+    },
+  },
+} as Options;
